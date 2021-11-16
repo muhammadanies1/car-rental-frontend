17c1b3aaca25e2a2f8a1f5b05b7059f848b4b8ab
Muhammad Anies Wahdie <manies5396@gmail.com>
2021-11-16 15:02:22 +0700

    [Member] - All components member done

---
+++ src/screens/member/RentCar.jsx
@@ -0,0 +1,68 @@
+import "./RentCar.css";
+import { Form, Container, Card, Button, Col, Row } from "react-bootstrap";
+import gambar from './mcqueen.jpg';
+import Modal from "react-bootstrap/Modal";
+import mapboxgl from 'mapbox-gl';
+import React, { useRef, useEffect, useState } from 'react';
+import FormRentCar from "../../components/modals/FormRentCar";
+
+const RentCar = () => {
+    
+    mapboxgl.accessToken = 'pk.eyJ1IjoibXVoYW1tYWQtYW5pZXMxIiwiYSI6ImNrdzFsOXdqamEzdGgzMHFwdXVncWdtengifQ.kulWbTY0gS5iPELC-iOWGA';
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(106.642164);
+    const [lat, setLat] = useState(-6.261058);
+    const [zoom, setZoom] = useState(10);
+    const [modalShow, setModalShow] = React.useState(false);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [lng, lat],
+        zoom: zoom
+        });
+        });
+
+    return(
+        <div>
+            <p className="name-partner">Jonathan RentCar</p>
+            <Row>
+                <Col sm={4}>
+                <Card className="card-detail-car" style={{ width: '25rem' }}>
+                    <Card.Img variant="top" src={gambar} />
+                    <Card.Body>
+                    <Row>
+                        <Col>
+                            <Card.Title className="name-car" >Avanza New Gen</Card.Title>
+                            <Card.Subtitle className="card-subtitle">Gading Serpong, Jonathan</Card.Subtitle>
+                        </Col>
+                        <Col md="auto">
+                            <Card.Text className="price"> Rp 100.000 </Card.Text>
+                            <Button className="btn-book" variant="primary" onClick={() => setModalShow(true)}>Book Now</Button></Col>
+                    </Row>
+                    </Card.Body>
+                </Card>
+                </Col>
+                <Col sm={8}>
+                    <Card className="card-maps">
+                        <Card.Body>
+                            <div>
+                                <div ref={mapContainer} className="map-container" />
+                            </div> 
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+
+            <FormRentCar
+        show={modalShow}
+        onHide={() => setModalShow(false)} />
+        </div>
+        
+    )
+}
+
+export default RentCar;
