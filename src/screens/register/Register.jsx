553d8bf81aaffd54c704e81b5adf38cdcfff7841
franciscaib <franciscaintanb99@gmail.com>
2021-11-17 10:26:03 +0700

    [intan] menambahkan fetching register ke backend

--- src/screens/register/Register.jsx
+++ src/screens/register/Register.jsx
@@ -1,15 +1,65 @@
-import { Card, FloatingLabel, Form, Button } from "react-bootstrap";
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Card, FloatingLabel, Form, Button } from "react-bootstrap";
 import "./Register.css";
 
 
 function Register() {
     let navigate = useNavigate();
 
+    const validName = new RegExp('[a-zA-Z]+');
+    const validUsername = new RegExp('[A-Za-z0-9]+[@]+[a-z]+[.]+[c][o][m]');
+    const validPhoneNumber = new RegExp('\\d+');
+
+    const [form, setForm] = useState({
+        username: "",
+        password: "",
+        name:"",
+        address:"",
+        phone_number:"",
+    });
+
+    function formHandler(events){
+        console.log(events.target.value);
+        return setForm({
+            ...form,
+            [events.target.name]: events.target.value,
+        });
+    };
+
     function toLogin(){
         navigate("/");
     }
 
+    function registerHandler(events){
+        events.preventDefault();
+        let username = form.username;
+        let password = form.password;
+        let name = form.name;
+        let address = form.address;
+        let phone_number = form.phone_number;
+
+            if(username === "" && password === "" && name ==="" && address==="" && phone_number === ""){
+                alert("tidak boleh kosong ya!");
+            }else if(!validName.test(name)){
+                alert("Nama harus menggunakan huruf");
+            }else if(!validUsername.test(username)){
+                alert("Username harus menggunakan email");
+            }else if(!validPhoneNumber.test(phone_number)){
+                alert("nomor telfon harus angka");
+            } else{
+                axios.post(`/api/register`,form).then((response) => {
+
+                    alert("Register berhasil.")
+                    navigate("/");        
+                }, (error) => {
+                    alert("Silahkan pilih username lain");
+                });
+            }
+    }
+
+    console.log(form);
     return (
         <Card className="card-daftar">
             <Card.Body>
@@ -17,21 +67,21 @@ function Register() {
                 <Card.Subtitle className="mb-2 text-muted login-subtitle">Enter your personal details to create account</Card.Subtitle>
                 <Form  className="d-grid gap-2">
                     <FloatingLabel controlId="floatingInput" label="Your Name" className="mb-2">
-                        <Form.Control type="text" placeholder="Enter your fullname" />
+                        <Form.Control name="name" type="text" placeholder="Enter your fullname" onChange={formHandler}/>
                     </FloatingLabel>
                     <FloatingLabel controlId="floatingInput" label="Address" className="mb-2">
-                        <Form.Control type="text" placeholder="Enter your address" />
+                        <Form.Control name="address" type="text" placeholder="Enter your address" onChange={formHandler}/>
                     </FloatingLabel>
                     <FloatingLabel controlId="floatingInput" label="Phone Number" className="mb-2">
-                        <Form.Control type="text" placeholder="Enter your phone number" />
+                        <Form.Control name="phone_number" type="text" placeholder="Enter your phone number" onChange={formHandler}/>
                     </FloatingLabel>
                     <FloatingLabel controlId="floatingInput" label="Username" className="mb-2">
-                        <Form.Control type="email" placeholder="Enter your username" />
+                        <Form.Control name="username" type="email" placeholder="Enter your username" onChange={formHandler}/>
                     </FloatingLabel>
                     <FloatingLabel controlId="floatingPassword" label="Password" className="mb-2">
-                        <Form.Control type="password" placeholder="Enter your password" />
+                        <Form.Control name="password" type="password" placeholder="Enter your password" onChange={formHandler}/>
                     </FloatingLabel>
-                    <Button variant="primary" size="lg"> SUBMIT </Button>
+                    <Button variant="primary" size="lg" onClick={registerHandler}> SUBMIT </Button>
                     <div className="to-login" onClick={toLogin}> Have an account? <strong>Login</strong> <i class="fas fa-arrow-right"></i> </div>
                 </Form>
             </Card.Body>
