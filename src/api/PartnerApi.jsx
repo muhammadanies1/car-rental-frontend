2194f0fb4144c5a451a27830b37c611eaa1b95ba
Muhammad Anies Wahdie <manies5396@gmail.com>
2021-11-19 13:26:20 +0700

    [RentCar] - Get Data Partner By Id and view to maps done

---
+++ src/api/PartnerApi.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+let token = localStorage.getItem("token");
+let user_id = localStorage.getItem("user_id");
+
+export function postJoinPartner(payload){
+    let fetching = axios.post(`/api/partner/add/${user_id}`,payload);
+    return fetching;
+}
+
+
--- src/screens/member/RentCar.jsx
+++ src/screens/member/RentCar.jsx
@@ -1,30 +1,26 @@
 import { Card, Button, Col, Row } from "react-bootstrap";
 import mapboxgl from 'mapbox-gl';
-import React, { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import FormRentCar from "../../components/modals/FormRentCar";
+import RoomIcon from "@mui/icons-material/Room";
 
 import gambar from './mcqueen.jpg';
 import "./RentCar.css";
+import axios from "axios";
 
 const RentCar = () => {
-    
-    mapboxgl.accessToken = 'pk.eyJ1IjoibXVoYW1tYWQtYW5pZXMxIiwiYSI6ImNrdzFsOXdqamEzdGgzMHFwdXVncWdtengifQ.kulWbTY0gS5iPELC-iOWGA';
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(106.642164);
-    const [lat, setLat] = useState(-6.261058);
-    const [zoom, setZoom] = useState(10);
-    const [modalShow, setModalShow] = React.useState(false);
+    const [modalShow, setModalShow] = useState(false);
+    const [partner, setPartner] = useState(null);
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
-            zoom: zoom
-            });
-        });
+    const [viewport, setViewport] = useState({
+        width: "57vw",
+        height: "73vh",
+        latitude: -6.261058,
+        longitude: 106.642164,
+        zoom: 10
+    });
+    
 
     return(
         <>
@@ -53,7 +49,44 @@ const RentCar = () => {
                     <Card className="card-maps">
                         <Card.Body>
                             <div>
-                                <div ref={mapContainer} className="map-container" />
+                            <ReactMapGL
+                    {...viewport}
+                    mapboxApiAccessToken="pk.eyJ1IjoibXVoYW1tYWQtYW5pZXMxIiwiYSI6ImNrdzFsOXdqamEzdGgzMHFwdXVncWdtengifQ.kulWbTY0gS5iPELC-iOWGA"
+                    onViewportChange={nextViewport => setViewport(nextViewport)}
+                    mapStyle="mapbox://styles/muhammad-anies1/ckw4bf05v0ezn14nuofdmayiv"
+                    >
+                    
+                    <Marker latitude={-6.261058} 
+                        longitude={106.642164} 
+                        offsetLeft={-20} 
+                        offsetTop={-10}>
+                        <RoomIcon style={{ fontSize:viewport.zoom * 5, color: "slateblue" }}/>
+                    </Marker>
+                    {/* { newCoordinate && (
+
+                        <Popup latitude={newCoordinate.lat} 
+                        longitude={newCoordinate.long} 
+                        closeButton={true} 
+                        closeOnClick={false} 
+                        anchor="left" 
+                        onClose={() => setNewCoordinate(null) }
+                        >
+                        <div className="card-pop-up">
+                            <form onSubmit={handleSubmit}>
+                                <label>Name Partner</label>
+                                    <input name="partner_name" 
+                                    placeholder="Enter your rent name"
+                                    onChange={handleForm} />
+                                <label>City</label>
+                                    <input name="city" 
+                                    placeholder="Enter your city"
+                                    onChange={handleForm} />
+                                <button className="submitButton" type="submit">Add Pin</button>
+                            </form>
+                        </div>
+                        </Popup>
+                    )} */}
+                </ReactMapGL>
                             </div> 
                         </Card.Body>
                     </Card>
