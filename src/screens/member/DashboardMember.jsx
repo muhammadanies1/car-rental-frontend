1b457bdaf0f2302f9d62320c7b38f57a4281d350
Jonathan Gabriel Budiman <jojgb96@gmail.com>
2021-11-20 14:42:20 +0700

    benerin dashboard

--- src/screens/member/DashboardMember.jsx
+++ src/screens/member/DashboardMember.jsx
@@ -22,9 +22,9 @@ function DashboardMember(props) {
     const user_id = JSON.parse(localStorage.getItem("user_id"));
     const [userTransaction, setUserTransaction] = useState(0);
 
-    function bookHandler(events) {
-        events.preventDefault();
-        navigate("/member/rentcar/"+cars[0].car_id);
+    function bookHandler(carId) {
+        // events.preventDefault();
+        navigate("/member/rentcar/"+carId);
         window.location.reload();
         
     }
@@ -39,17 +39,17 @@ function DashboardMember(props) {
 
         axios.get(`/api/process/${user_id}`)
             .then(res => {
-                console.log(res.data.data[0]);
-                setCarProcess(res.data.data[0]);
-                if(res.data.data[0] != null){
-                    // console.log("masuk sini");
-                    // setUserTransaction(res.data.data[0].user.user_id);
+                console.log(res.data.data);
+                // console.log(user_id);
+                setCarProcess(res.data.data);
+                if(carProcess != null){
+                    setUserTransaction(res.data.data.user.user_id);
                 }
                 // dispatch(carActions.getAllCarTrue(res.data))
                 // setCars(res.data.data)
             })
     }, [dispatch])
-
+    console.log(userTransaction);
     function searchHandler(events) {
         setSearch(events.target.value);
     };
@@ -122,7 +122,7 @@ function DashboardMember(props) {
                                         </Col>
                                         <Col md="auto">
                                             <Card.Text className="car-price"> Rp {value.price} </Card.Text>
-                                            <Button className="btn-book" variant="primary" onClick={bookHandler}>Book Now</Button></Col>
+                                            <Button className="btn-book" variant="primary" onClick={()=>bookHandler(value.car_id)}>Book Now</Button></Col>
                                     </Row>
                                 </Card.Body>
                             </Card>
