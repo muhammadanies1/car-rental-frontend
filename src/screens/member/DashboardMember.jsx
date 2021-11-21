27ff9670d5bc78f1e24c6c3c499e5f8a07dbe3b2
Muhammad Anies Wahdie <manies5396@gmail.com>
2021-11-21 14:22:35 +0700

    status finish car

--- src/screens/member/DashboardMember.jsx
+++ src/screens/member/DashboardMember.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
 import { carActions } from "../../store/car";
+import StatusFinish from "./ForDashboard/StatusFinish";
 import "./DashboardMember.css";
 import axios from "axios";
 
@@ -16,7 +17,6 @@ function DashboardMember() {
     const [carProcess, setCarProcess] = useState(null);
     const user_id = JSON.parse(localStorage.getItem("user_id"));
     const [userTransaction, setUserTransaction] = useState();
-    const [paidStatus, setPaidStatus] = useState("");
 
     function bookHandler(carId) {
         // events.preventDefault();
@@ -46,15 +46,15 @@ function DashboardMember() {
         setSearch(events.target.value);
     };
 
-    function buttonSearchHandler(events) {
-        axios.get(`/api/car/city/` + search)
-            .then(res => {
-                dispatch(carActions.getAllCarTrue(res.data.data))
-                setCars(res.data.data)
-            }, (error) => {
-                // alert("Tidak ada");
-            })
-    }
+    // function buttonSearchHandler(events) {
+    //     axios.get(`/api/car/city/` + search)
+    //         .then(res => {
+    //             dispatch(carActions.getAllCarTrue(res.data.data))
+    //             setCars(res.data.data)
+    //         }, (error) => {
+    //             // alert("Tidak ada");
+    //         })
+    // }
 
     function buttonReturnAndPaymentHandler(events) {
         console.log(carProcess.transaction_id);
@@ -75,62 +75,7 @@ function DashboardMember() {
 
     return (
         <>
-            {userTransaction == user_id ?
-            <div id="card-with-status">
-                <h4>Your Transaction</h4>
-                <hr />
-                <Card className="card-car" style={{ width: '25rem' }}>
-                <Card.Img className="card-img" variant="top" src={carProcess.car.image} />
-                <Card.Body>
-                    <Row>
-                        <Col>
-                            <Card.Title className="car-name">{carProcess.car.merk}</Card.Title>
-                            <Card.Subtitle className="card-subtitle">{carProcess.car.partner.city}, {carProcess.car.partner.partner_name}</Card.Subtitle>
-                        </Col>
-                        <Col md="auto">
-                            <Card.Text className="car-price"> Rp {carProcess.total_payment + carProcess.penalty} </Card.Text>
-                            <Button className="btn-book" variant="primary" onClick={buttonReturnAndPaymentHandler}>
-                                {carProcess.paid_status == "Reserved" ? <>Return</> : 
-                                carProcess.paid_status == "Return" ? <>Waiting payment</> :
-                                carProcess.paid_status == "Waiting for payment" ? <>payment</> :
-                                <>Paid off</>}</Button>
-                            </Col>
-                    </Row>
-                </Card.Body>
-                </Card>
-            </div>
-                :
-                <>
-                <div id="container-dashboard">
-                    <Form className="d-flex search-input">
-                        <FormControl type="search" placeholder="I need a car at" className="me-2" aria-label="Search" onChange={searchHandler} />
-                        <Button variant="outline-success" onClick={buttonSearchHandler}> Search </Button>
-                    </Form>
-                    <h4>More than 100+ cars</h4>
-                    <hr className="more"/>
-                    <div id="container-car">
-                    {cars.map((value) => {
-                        return (
-                            <Card className="card-car" style={{ width: '25rem' }}>
-                                <Card.Img className="card-img" variant="top" src={value.image} />
-                                <Card.Body>
-                                    <Row>
-                                        <Col>
-                                            <Card.Title className="car-name">{value.merk}</Card.Title>
-                                            <Card.Subtitle className="card-subtitle">{value.partner.city}, {value.partner.partner_name}</Card.Subtitle>
-                                        </Col>
-                                        <Col md="auto">
-                                            <Card.Text className="car-price"> Rp {value.price} </Card.Text>
-                                            <Button className="btn-book" variant="primary" onClick={()=>bookHandler(value.car_id)}>Book Now</Button></Col>
-                                    </Row>
-                                </Card.Body>
-                            </Card>
-                        )
-                    })}
-                    </div>
-                </div>
-                </>
-            }
+            <StatusFinish cars={cars} />
         </>
 
     )
