cb031da0ea5b1a2ec42529e80fe6bb35e48943ad
Muhammad Anies Wahdie <manies5396@gmail.com>
2021-11-24 09:39:24 +0700

    menambahkan landingpage

---
+++ src/components/landingpage/Landingpage.jsx
@@ -0,0 +1,19 @@
+import "./Landingpage.css";
+import travel_01 from "../../assets/travel-01.jpg";
+import travel_02 from "../../assets/travel-02.jpg";
+import travel_03 from "../../assets/travel-03.jpg";
+
+function Landingpage(){
+    const navbarLinks = [
+        { url: "#", title: "Home" },
+        { url: "#", title: "About Us" },
+        { url: "#", title: "Login" },
+        
+    ];
+
+    return(
+        <div className="container-Landing">
+            
+        </div>
+    )
+}
