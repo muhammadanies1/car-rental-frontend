71b69ee212c0a3e16966d4bceb6e56f34030d4d1
franciscaib <franciscaintanb99@gmail.com>
2021-11-24 14:43:29 +0700

    [intan] modifikasi register

--- src/screens/register/Register.jsx
+++ src/screens/register/Register.jsx
@@ -17,12 +17,12 @@ function Register() {
     const [form, setForm] = useState({
         username: "",
         password: "",
-        name:"",
-        address:"",
-        phone_number:"",
+        name: "",
+        address: "",
+        phone_number: "",
     });
 
-    function formHandler(events){
+    function formHandler(events) {
         console.log(events.target.value);
         return setForm({
             ...form,
@@ -30,11 +30,11 @@ function Register() {
         });
     };
 
-    function toLogin(){
+    function toLogin() {
         navigate("/");
     }
 
-    function registerHandler(events){
+    function registerHandler(events) {
         events.preventDefault();
         let username = form.username;
         let password = form.password;
@@ -42,53 +42,55 @@ function Register() {
         let address = form.address;
         let phone_number = form.phone_number;
 
-            if(username === "" && password === "" && name ==="" && address==="" && phone_number === ""){
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Please complete the registration data',
-                    showConfirmButton: false,
-                    timer: 2000,
-                    timerProgressBar: true,
-                });
-            }else if(!validName.test(name)){
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Please enter name using letter characters',
-                    showConfirmButton: false,
-                    timer: 2000,
-                    timerProgressBar: true,
-                });
-            }else if(!validUsername.test(username)){
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Please enter username using email',
-                    showConfirmButton: false,
-                    timer: 2000,
-                    timerProgressBar: true,
-                });
-            }else if(!validPhoneNumber.test(phone_number)){
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Please enter phone number using numbers',
-                    showConfirmButton: false,
-                    timer: 2000,
-                    timerProgressBar: true,
-                });
-            } else if(!validPassword.test(password)){
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Password must be at least 6 characters including one uppercase letter, one lowercase letter,  one number and one special character',
-                    showConfirmButton: false,
-                    timer: 2000,
-                    timerProgressBar: true,
-                });
-            } else{
-                axios.post(`/api/register`,form).then((response) => {
+        if (username === "" && password === "" && name === "" && address === "" && phone_number === "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Please complete the registration data',
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+            });
+        } else if (!validName.test(name)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Please enter name using letter characters',
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+            });
+        } else if (!validUsername.test(username)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Please enter username using email',
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+            });
+        } else if (!validPhoneNumber.test(phone_number)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Please enter phone number using numbers',
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+            });
+        } else if (!validPassword.test(password)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Password must be at least 6 characters including one uppercase letter, one lowercase letter,  one number and one special character',
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+            });
+        } else {
+            axios
+                .post(`/api/register`, form)
+                .then((response) => {
                     Swal.fire({
                         icon: 'success',
                         title: 'Done!',
@@ -97,9 +99,9 @@ function Register() {
                         timer: 3000,
                         timerProgressBar: true,
                     });
-                    
-                    navigate("/");        
-                }, (error) => {
+                    navigate("/");
+                })
+                .catch((error) => {
                     Swal.fire({
                         icon: 'error',
                         title: 'Oops...',
@@ -108,8 +110,8 @@ function Register() {
                         timer: 3000,
                         timerProgressBar: true,
                     });
-                });
-            }
+                })
+        }
     }
 
     console.log(form);
@@ -118,21 +120,21 @@ function Register() {
             <Card.Body>
                 <Card.Title className="title-daftar">Create your account</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted login-subtitle">Enter your personal details to create account</Card.Subtitle>
-                <Form  className="d-grid gap-2">
+                <Form className="d-grid gap-2">
                     <FloatingLabel controlId="floatingInput" label="Your Name" className="mb-2">
-                        <Form.Control name="name" type="text" placeholder="Enter your fullname" onChange={formHandler}/>
+                        <Form.Control name="name" type="text" placeholder="Enter your fullname" onChange={formHandler} />
                     </FloatingLabel>
                     <FloatingLabel controlId="floatingInput" label="Address" className="mb-2">
-                        <Form.Control name="address" type="text" placeholder="Enter your address" onChange={formHandler}/>
+                        <Form.Control name="address" type="text" placeholder="Enter your address" onChange={formHandler} />
                     </FloatingLabel>
                     <FloatingLabel controlId="floatingInput" label="Phone Number" className="mb-2">
-                        <Form.Control name="phone_number" type="text" placeholder="Enter your phone number" onChange={formHandler}/>
+                        <Form.Control name="phone_number" type="text" placeholder="Enter your phone number" onChange={formHandler} />
                     </FloatingLabel>
                     <FloatingLabel controlId="floatingInput" label="Username" className="mb-2">
-                        <Form.Control name="username" type="email" placeholder="Enter your username" onChange={formHandler}/>
+                        <Form.Control name="username" type="email" placeholder="Enter your username" onChange={formHandler} />
                     </FloatingLabel>
                     <FloatingLabel controlId="floatingPassword" label="Password" className="mb-2">
-                        <Form.Control name="password" type="password" placeholder="Enter your password" onChange={formHandler}/>
+                        <Form.Control name="password" type="password" placeholder="Enter your password" onChange={formHandler} />
                     </FloatingLabel>
                     <Button variant="primary" size="lg" onClick={registerHandler}> SUBMIT </Button>
                     <div className="to-login" onClick={toLogin}> Have an account? <strong>Login</strong> <i class="fas fa-arrow-right"></i> </div>
